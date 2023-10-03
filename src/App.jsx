4fe15d4d404649ebe74c45d57c2d1083429ae5f1
JamieYau <jamie.yau2002@me.com>
2023-10-04 00:16:34 +0100

    Add dynamic rendering of sections

--- src/App.jsx
+++ src/App.jsx
@@ -2,105 +2,114 @@ import "./App.css";
 import FormSection from "./components/FormSection";
 
 function App() {
-  const generalInfoFields = [
+  const sections = [
     {
-      label: "Full Name",
-      name: "fullName",
-      type: "text",
-      initialValue: "",
-      required: true,
+      heading: "General Information",
+      fields: [
+        {
+          label: "Full Name",
+          name: "fullName",
+          type: "text",
+          initialValue: "",
+          required: true,
+        },
+        {
+          label: "Email",
+          name: "email",
+          type: "email",
+          initialValue: "",
+          required: true,
+        },
+        {
+          label: "Phone",
+          name: "phone",
+          type: "tel",
+          initialValue: "",
+          required: true,
+        },
+        {
+          label: "Address",
+          name: "address",
+          type: "text",
+          initialValue: "",
+          required: false,
+        },
+      ],
     },
     {
-      label: "Email",
-      name: "email",
-      type: "email",
-      initialValue: "",
-      required: true,
+      heading: "Education",
+      fields: [
+        {
+          label: "Degree",
+          name: "degree",
+          type: "text",
+          initialValue: "",
+          required: true,
+        },
+        {
+          label: "Institution",
+          name: "institution",
+          type: "text",
+          initialValue: "",
+          required: true,
+        },
+        {
+          label: "Year",
+          name: "year",
+          type: "number",
+          initialValue: "",
+          required: true,
+        },
+      ],
     },
     {
-      label: "Phone",
-      name: "phone",
-      type: "tel",
-      initialValue: "",
-      required: true,
-    },
-    {
-      label: "Address",
-      name: "address",
-      type: "text",
-      initialValue: "",
-      required: false,
-    },
-  ];
-
-  const educationFields = [
-    {
-      label: "Degree",
-      name: "degree",
-      type: "text",
-      initialValue: "",
-      required: true,
-    },
-    {
-      label: "Institution",
-      name: "institution",
-      type: "text",
-      initialValue: "",
-      required: true,
-    },
-    {
-      label: "Year",
-      name: "year",
-      type: "number",
-      initialValue: "",
-      required: true,
-    },
-  ];
-
-  const experienceFields = [
-    {
-      label: "Company",
-      name: "company",
-      type: "text",
-      initialValue: "",
-      required: true,
-    },
-    {
-      label: "Position",
-      name: "position",
-      type: "text",
-      initialValue: "",
-      required: true,
-    },
-    {
-      label: "Description",
-      name: "description",
-      type: "textarea",
-      initialValue: "",
-      required: true,
-    },
-    {
-      label: "Start Date",
-      name: "startDate",
-      type: "date",
-      initialValue: "",
-      required: true,
-    },
-    {
-      label: "End Date",
-      name: "endDate",
-      type: "date",
-      initialValue: "",
-      required: false,
+      heading: "Experience",
+      fields: [
+        {
+          label: "Company",
+          name: "company",
+          type: "text",
+          initialValue: "",
+          required: true,
+        },
+        {
+          label: "Position",
+          name: "position",
+          type: "text",
+          initialValue: "",
+          required: true,
+        },
+        {
+          label: "Description",
+          name: "description",
+          type: "textarea",
+          initialValue: "",
+          required: true,
+        },
+        {
+          label: "Start Date",
+          name: "startDate",
+          type: "date",
+          initialValue: "",
+          required: true,
+        },
+        {
+          label: "End Date",
+          name: "endDate",
+          type: "date",
+          initialValue: "",
+          required: false,
+        },
+      ],
     },
   ];
 
   return (
     <div className="app">
       <h1>CV Builder</h1>
-      <FormSection heading="General Information" fields={generalInfoFields} />
-      <FormSection heading="Education" fields={educationFields} />
-      <FormSection heading="Experience" fields={experienceFields} />
+      {sections.map((section) => (
+        <FormSection key={section.heading} {...section} />
+      ))}
     </div>
   );
 }
