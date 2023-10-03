64495fbff38a5d7311aa7d9f90ba2cea441f09ac
JamieYau <jamie.yau2002@me.com>
2023-10-03 21:01:03 +0100

    Add education & Experience sections

--- src/App.jsx
+++ src/App.jsx
@@ -33,10 +33,67 @@ function App() {
     },
   ];
 
+  const educationFields = [
+    {
+      label: "Degree",
+      name: "degree",
+      type: "text",
+      initialValue: "",
+      required: true,
+    },
+    {
+      label: "Institution",
+      name: "institution",
+      type: "text",
+      initialValue: "",
+      required: true,
+    },
+    {
+      label: "Year",
+      name: "year",
+      type: "number",
+      initialValue: "",
+      required: true,
+    },
+  ];
+
+  const experienceFields = [
+    {
+      label: "Company",
+      name: "company",
+      type: "text",
+      initialValue: "",
+      required: true,
+    },
+    {
+      label: "Position",
+      name: "position",
+      type: "text",
+      initialValue: "",
+      required: true,
+    },
+    {
+      label: "Start Date",
+      name: "startDate",
+      type: "date",
+      initialValue: "",
+      required: true,
+    },
+    {
+      label: "End Date",
+      name: "endDate",
+      type: "date",
+      initialValue: "",
+      required: false,
+    },
+  ];
+
   return (
     <div className="app">
       <h1>CV Builder</h1>
       <FormSection heading="General Information" fields={generalInfoFields} />
+      <FormSection heading="Education" fields={educationFields} />
+      <FormSection heading="Experience" fields={experienceFields} />
     </div>
   );
 }
